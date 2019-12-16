79346f1387993802b5e80a816e3798e644b7547d
primoretc <primoretc@gmail.com>
2019-12-16 22:25:22 +0300

    код кешы

--- python/kesha.py
+++ python/kesha.py
@@ -1,5 +1,3 @@
-
-
 # Голосовой ассистент КЕША 1.0 BETA
 import os
 import time
@@ -7,8 +5,8 @@ import speech_recognition as sr
 from fuzzywuzzy import fuzz
 import pyttsx3
 import datetime
-
-# Настройки
+ 
+# настройки
 opts = {
     "alias": ('кеша','кеш','инокентий','иннокентий','кишун','киш',
               'кишаня','кяш','кяша','кэш','кэша'),
@@ -16,86 +14,88 @@ opts = {
     "cmds": {
         "ctime": ('текущее время','сейчас времени','который час'),
         "radio": ('включи музыку','воспроизведи радио','включи радио'),
-        "stupid1": ('расскажи анекдот','рассмеши меня','ты знаешь анекдот'),
+        "stupid1": ('расскажи анекдот','рассмеши меня','ты знаешь анекдоты')
     }
 }
-
-# Функции
+ 
+# функции
 def speak(what):
     print( what )
     speak_engine.say( what )
     speak_engine.runAndWait()
     speak_engine.stop()
-
+ 
 def callback(recognizer, audio):
     try:
         voice = recognizer.recognize_google(audio, language = "ru-RU").lower()
         print("[log] Распознано: " + voice)
-
+   
         if voice.startswith(opts["alias"]):
-            # Обращаются к Кеше
+            # обращаются к Кеше
             cmd = voice
-
+ 
             for x in opts['alias']:
                 cmd = cmd.replace(x, "").strip()
-
+           
             for x in opts['tbr']:
                 cmd = cmd.replace(x, "").strip()
-
-            # Распознаём и выполняем команду
+           
+            # распознаем и выполняем команду
             cmd = recognize_cmd(cmd)
             execute_cmd(cmd['cmd'])
-
+ 
     except sr.UnknownValueError:
         print("[log] Голос не распознан!")
     except sr.RequestError as e:
-        print("[log] Неизвестная ошибка, проверь интернет!")
-
+        print("[log] Неизвестная ошибка, проверьте интернет!")
+ 
 def recognize_cmd(cmd):
     RC = {'cmd': '', 'percent': 0}
     for c,v in opts['cmds'].items():
-
+ 
         for x in v:
             vrt = fuzz.ratio(cmd, x)
             if vrt > RC['percent']:
                 RC['cmd'] = c
                 RC['percent'] = vrt
-
+   
     return RC
-
+ 
 def execute_cmd(cmd):
     if cmd == 'ctime':
         # сказать текущее время
         now = datetime.datetime.now()
         speak("Сейчас " + str(now.hour) + ":" + str(now.minute))
-
+   
     elif cmd == 'radio':
-        # рассказать анекдот
-        speak("Мой разработчик, не научил меня радио ... Ха ха ха")
-
+        # воспроизвести радио
+        os.system("D:\\Jarvis\\res\\radio_record.m3u")
+   
     elif cmd == 'stupid1':
         # рассказать анекдот
-        speak("Мой разработчик, не научил меня анекдотам... Ха ха ха")
-
+        speak("Мой разработчик не научил меня анекдотам ... Ха ха ха")
+   
     else:
         print('Команда не распознана, повторите!')
-
-# Запуск
+ 
+# запуск
 r = sr.Recognizer()
 m = sr.Microphone(device_index = 1)
-
+ 
 with m as source:
     r.adjust_for_ambient_noise(source)
-
+ 
 speak_engine = pyttsx3.init()
-
-# Только если установлены голоса для синтеза речи!
+ 
+# Только если у вас установлены голоса для синтеза речи!
 voices = speak_engine.getProperty('voices')
 speak_engine.setProperty('voice', voices[4].id)
-
-speak("Добрый день, повелитель")
-speak("Кеша слушает")
-
-stop_listening = r.listen_in_background(m, callback)
-while True: time.sleep(0.1)
-
+ 
+# forced cmd test
+speak("Мой разработчик не научил меня анекдотам ... Ха ха ха")
+ 
+#speak("Добрый день, повелитель")
+#speak("Кеша слушает")
+ 
+#stop_listening = r.listen_in_background(m, callback)
+#while True: time.sleep(0.1) # infinity loop
