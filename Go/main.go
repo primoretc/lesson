1a7efb558f63211742d5a53a1e6bd0d418ffe56d
primoretc <primoretc@gmail.com>
2019-12-17 23:34:30 +0300

    сумма числе готово

--- Go/main.go
+++ Go/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
-func summa(x int, y int) {
-	var z = x + y
-	fmt.Println(z)
+func summa(x int, y int) (z int) {
+	z = x + y
+	//fmt.Println(z)
+	return z
 }
 
 func main() {
@@ -16,6 +18,9 @@ func main() {
 	fmt.Scanln(&x)
 	fmt.Println("Введите второе число ")
 	fmt.Scanln(&y)
-	summa(x, y)
+	//s := summa(x, y)
+	st1 := strconv.Itoa(x)
+	st2 := strconv.Itoa(y)
+	fmt.Println("Сумма двух чисел "+st1+" и "+st2+" равна", summa(x, y))
 
 }
