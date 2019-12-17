f57cfdf31b906f4c1e227391927b130ce16cc8e2
primoretc <primoretc@gmail.com>
2019-12-17 22:57:09 +0300

    summa

--- Go/main.go
+++ Go/main.go
@@ -1,7 +1,21 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+)
+
+func summa(x int, y int) {
+	var z = x + y
+	fmt.Println(z)
+}
 
 func main() {
-	fmt.Println("Hello Go")
+	var x int
+	var y int
+	fmt.Println("Введите первое число ")
+	fmt.Scanln(&x)
+	fmt.Println("Введите второе число ")
+	fmt.Scanln(&y)
+	summa(x, y)
+
 }
