e661daedcaab0f98ad7af68b37ba286a9444cf20
primoretc <primoretc@gmail.com>
2020-01-21 10:16:24 +0300

    Delete begin12.py
    del

--- python/begin12.py
+++
@@ -1,8 +0,0 @@
-# Даны каеты прямоугольного треугольника a b 
-# найти его гипотенузу c и периметр
-import math
-a = float(input("Введите катет a "))
-b = float(input("Введите катет b "))
-c=math.sqrt(math.pow(a,2)+math.pow(b,2))
-print("c=", c)
-print("P=", a+b+c)
